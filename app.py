bf924edb11e0f203491191d3c6a34d030a664dda
manmood69-prog <manmood69@gmail.com>
2026-05-16 18:05:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from tensorflow import keras
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-MODELS_FOLDER = os.path.join(BASE_DIR, 'models')
+MODELS_FOLDER = os.path.join(BASE_DIR)
 STATIC_FOLDER = os.path.join(BASE_DIR, 'static')
 PHOTOS_FOLDER = os.path.join(BASE_DIR, 'photos')
 
